b03cde11bd2231b871d6c057618aba3d86d9dd65
jondoesntgit <jonathan.m.wheeler@gmail.com>
2018-01-12 13:50:28 -0800

    Code cleanup

--- hardware/data_acquisition_units.py
+++ hardware/data_acquisition_units.py
@@ -104,18 +104,13 @@ class NI_9215:
             Using the ``next(g)`` syntax will yield a chunk of data of size
             rate * seconds
         """
-
-        print('start read')
-
         rate = self._get_rate(rate, oversampling_ratio)
         max_voltage = self._get_max_voltage(max_voltage)
  
-        print('define task')
 
         self.task = self.FOG_DAQ_Task(
             self, seconds, rate, oversampling_ratio, max_voltage, asynchronous)
 
-        print('starting task')
         if asynchronous:
             self.queue = queue.Queue()
             self.task.StartTask()
@@ -132,9 +127,6 @@ class NI_9215:
             self.task.ClearTask()
             self.task = None
             return self.data
-
-
-
 
     class FOG_DAQ_Task(Task):
         def __init__(self, daq, seconds, rate, oversampling_ratio, max_voltage, asynchronous):
@@ -276,118 +268,7 @@ class NI_9215:
 
         def finish(self, status):
             return 0 # The function should return an integer)
-
-    def data_stream(self):
-        """A generator that returns chunks of data"""
-        while True:
-            yield self.queue.get()
-
-    class CallbackTask(Task):
-        def __init__(self, rate, chunk_seconds, oversampling_ratio, max_voltage, device_name, timeout):
-
-            self.timeout = timeout
-            self.max_voltage = max_voltage
-            self.oversampling_ratio = oversampling_ratio
-
-            Task.__init__(self)
-
-            self.queue = queue.Queue()
-            self.chunk_size = int(chunk_seconds * rate * oversampling_ratio)
-            self.data = numpy.zeros(self.chunk_size)
-            self.CreateAIVoltageChan("%s/ai0" % device_name, "", DAQmx_Val_Diff ,-10, 10, DAQmx_Val_Volts,None)
-            self.CfgSampClkTiming("", rate*oversampling_ratio ,DAQmx_Val_Rising,DAQmx_Val_ContSamps, self.chunk_size)
-
-            # Map EveryNCallback and DoneCallback into C callback functions
-            self.AutoRegisterEveryNSamplesEvent(DAQmx_Val_Acquired_Into_Buffer,self.chunk_size,0)
-            self.AutoRegisterDoneEvent(0)
             
-
-    def async_read(self, chunk_seconds=1, rate=None, max_voltage=None, timeout=0,
-             verbose=False, oversampling_ratio=10, task_name=""):
-        """
-        Parameters
-        ----------
-        seconds : int, float
-            Sample duration in seconds
-        rate : int, float
-            Number of samples to collect per second. If not specified, will
-            default to the maximum of either 2 or the reciprocal of the
-            detected lock-in amplifier's time constant.
-        max_voltage : float
-            Scale factor. The DAQ expects a range of voltages ranging from -10V
-            to +10V. The max_voltage tells ``read()`` what to scale a 10V
-            signal by. If not specified, will default to the lock-in
-            amplifier's sensitivity.
-        timeout : int, float
-            Amount of time after which the program should timeout and give up
-            on collecting data.
-
-            - 0 (default) - calculate the timeout based on duration and
-                sample rate
-            - -1 - do not time out.
-            - other - the timeout
-        verbose : bool
-            If set to ``True``, this function will output any assumptions made
-            about the ``rate`` or ``max_voltage`` parameters.
-        oversampling_ratio : int
-            If N samples of the same quantity are taken, each with 
-            uncorrelated errors, averaging these values will reduce the noise
-            by a factor of :math:`\sqrt{N}`. By default, the output rate of 
-            the data returned by 'read' is 1/10 the bandwidth of the lock-in
-            amplifier. We can reduce our noise to a theoretical limit by sampling
-            at the lock-in amplifier bandwidth, and then downsampling via simple
-            averages of size N.
-
-        Returns
-        -------
-        numpy.array
-            A one-dimensional numpy array of the data.
-        """
-
-        # gather the rate from the inputs
-        if rate and rate < 2:
-            raise ValueError("Frequency needs to be > 2 Hz. You gave %f" % rate)
-        elif rate:
-            # use the passed rate
-            pass
-        elif self.rate:
-            rate = self.rate
-        else:
-            from hardware import lia
-            rate = .1/lia.time_constant
-            rate = max(2, rate)
-            if verbose:
-                print("Auto-setting sampling rate to %2g Hz based on "
-                "LIA settings" % rate)
-
-        # gather the maximum voltage from the inputs
-        if max_voltage:
-            # used the passed max_voltage
-            pass
-        elif self.max_voltage:
-            max_voltage = self.max_voltage
-        else:
-            from hardware import lia
-            max_voltage = lia.sensitivity
-            max_voltage_string = "%2g V"
-            if max_voltage > .001:
-                max_voltage_string = "%2g mV" % (max_voltage * 1e3)
-            elif max_voltage > 1e-6:
-                max_voltage_string = "%2g uV" % (max_voltage * 1e6)
-            elif max_voltage > 1e-9:
-                max_voltage_string = "%2g nV" % (max_voltage * 1e9)
-            if verbose:
-                print("Auto-setting max_voltage to %s based on "
-                "LIA settings" % max_voltage_string)
-
-        chunk_sample_size = int(chunk_seconds * rate * oversampling_ratio)
-
-        self.task = self.CallbackTask(rate, chunk_seconds, oversampling_ratio, max_voltage, self.device_name, timeout)
-        self.task.StartTask()
-
-    def data_stream(self):
-        return self.task.data_stream()
-
     def stop(self):
         """ If running in asynchronous mode, this stops the task"""
         self.task.StopTask()
