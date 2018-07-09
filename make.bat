d33509bfe2aed49ca947cb0191f881607f6692d3
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2018-07-09 16:15:24 -0700

    Updated make file, and fixed filename change in logging

--- make.bat
+++ make.bat
@@ -3,8 +3,27 @@
 REM Command file for running tests
 pushd %~dp0
 
+if "%1" == "" goto help
 if "%1" == "test" goto test
-if "%1" == "upload_tests" goto tests
+if "%1" == "tests" goto test
+if "%1" == "upload" goto upload_tests
+if "%1" == "upload_tests" goto upload_tests
+
+echo."%1" is not a valid recipe. Type "make" for help
+goto end
+
+:help
+
+echo.
+echo.Usage:
+echo.
+echo.make test
+echo.    Runs pytest to evaluate all tests in the test/ directory
+echo.
+echo.make upload_tests
+echo.    Runs tests, then uploads results to codecov
+
+goto end
 
 :test
 py.test
