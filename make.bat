3f1a071bd4a64de58a4a331f50bde01ee25dfe8c
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2018-07-09 14:27:50 -0700

    Updated the make file

--- make.bat
+++ make.bat
@@ -1,3 +1,19 @@
-tests:
-	coverage run --source hardware setup.py test
-	codecov --token=d7078188-16c9-4e86-8d10-2b3c9f1aa992
+@ECHO OFF
+
+REM Command file for running tests
+pushd %~dp0
+
+if "%1" == "test" goto test
+if "%1" == "upload_tests" goto tests
+
+:test
+py.test
+goto end
+
+:upload_tests
+coverage run --source hardware setup.py test
+codecov --token=d7078188-16c9-4e86-8d10-2b3c9f1aa992
+goto end
+
+:end
+popd
