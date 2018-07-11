d323fb8cc0338f77ef054b135686af53bfbf04f5
Anjali Thontakudi <anjali.thontakudi@gmail.com>
2018-07-10 18:11:28 -0700

    Tests for LIA (take 1)

--- tests/test_lock_in_amplifier.py
+++ tests/test_lock_in_amplifier.py
@@ -1,49 +1,48 @@
 from hardware import lia
 from hardware import u
-from hardware import file
+from hardware import log_filename
 
 
 def test_phase():
     # units in degrees
-    assert lia.phase.units == "degree"
+    phase = 1 * u.hectodegree
+    lia.phase = phase
+    assert lia.phase.units == u.degree
 
-    # Must be between -180 to 180 degrees
     try:
-        lia.phase = 200 * u.degree
+        lia.phase = 600 * u.degree
     except(ValueError):
-        print("Invalid phase setting successfully caught in lia")
-
-    lia.phase = 100 * u.degree
+        print("Invalid phase successfully caught in lia")
 
     # change in logging file
-    assert ("Phase set to %f" % lia.phase.magnitude) in file.read()
+    with(open(log_filename)) as log_file:
+        assert ("Phase set to %f degrees." % lia.phase.magnitude) in log_file.read()
 
 
 def test_sensitivity():
     # units in volts
-    assert lia.sensitivity.units == "volt"
-
-    # Sensitivity must be within the length of the sensitivity dictionary
-    try:
-        lia.sensitivity = 30
-    except(ValueError):
-        print("Invalid sensitivity successfully caught in lia")
-
-    lia.sensitvity = 5
+    sensitivity = .1 * u.hectovolt
+    lia.sensitvity = sensitivity
+    assert lia.sensitivity.units == u.volt
 
     # change in logging file
-    assert ("Sensitivity set to %f" % lia.sensitivity.magnitude) in file.read()
+    with(open(log_filename)) as log_file:
+        assert ("Sensitivity set to %f V." % lia.sensitivity.magnitude) in log_file.read()
 
 
 def test_identify():
-    print(lia.indentify())
+    print(lia.identify())
 
 
 def test_time_const():
-    assert lia.time_constant.units == "second"
+    time_const = 3 * u.second
+    lia.time_constant = time_const
+    assert lia.time_constant.units == u.second
+
     try:
         lia.time_constant = 2 * u.second
-    except:
-        print("Invalid time constant succesfully caught in lia")
-    lia.time_constant = 3 * u.second
-    assert ("Time constant set to %f" % lia.time_constant.magnitude) in file.read()
+    except(ValueError):
+        print("Invalid phase successfully caught in lia")
+
+    with(open(log_filename)) as log_file:
+        assert ("Time constant set to %f seconds." % lia.time_constant.magnitude) in log_file.read()
