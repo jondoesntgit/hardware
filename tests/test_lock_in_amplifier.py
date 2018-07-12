c3ef397d68907184fe8f1cde1134f8e8a89458f8
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2018-07-12 11:25:27 -0700

    Fixed lock-in amplifier test

--- tests/test_lock_in_amplifier.py
+++ tests/test_lock_in_amplifier.py
@@ -1,52 +1,68 @@
 """A set of functions that test the LIA."""
 import pytest
 try:
-    from hardware import u, log_filename, lia
+    from hardware import Q_, u, log_filename, lia
 except ImportError:
     pytestmark = pytest.mark.skip
 
 
 def test_phase():
+    """Test the LIA phase functions."""
     # units in degrees
-    assert lia.phase.units == "degree"
+    start_phase = lia.phase
+    assert start_phase.units == "degree"
 
     # Must be between -180 to 180 degrees
-    try:
+    with pytest.raises(ValueError):
         lia.phase = 200 * u.degree
-    except(ValueError):
-        print("Invalid phase setting successfully caught in lia")
 
     lia.phase = 100 * u.degree
 
     # change in logging file
-    assert ("Phase set to %f" % lia.phase.magnitude) in file.read()
+    with open(log_filename) as file:
+        string = "Phase set to %f" % lia.phase.magnitude
+        assert string in file.read()
+
+    # Restore
+    lia.phase = start_phase
 
 
 def test_sensitivity():
-    # units in volts
-    assert lia.sensitivity.units == "volt"
+    """Test the LIA sensitivity."""
+    start_sensitivity = lia.sensitivity
+    assert start_sensitivity.units == "volt"
 
     # Sensitivity must be within the length of the sensitivity dictionary
-    try:
-        lia.sensitivity = 30
-    except(ValueError):
-        print("Invalid sensitivity successfully caught in lia")
+    with pytest.raises(ValueError):
+        lia.sensitivity = Q_(30, 'volt')
 
-    lia.sensitvity = 5
+    lia.sensitivity = Q_(.1, 'volt')
 
     # change in logging file
-    assert ("Sensitivity set to %f" % lia.sensitivity.magnitude) in file.read()
+    with open(log_filename) as file:
+        string = "Sensitivity set to %f V." % lia.sensitivity.magnitude
+        assert string in file.read()
+
+    # Restore
+    lia.sensitivity = start_sensitivity
 
 
 def test_identify():
-    print(lia.indentify())
+    """Ensure LIA can identify itself and its serial numbers."""
+    print(lia.identify())
 
 
-def test_time_const():
-    assert lia.time_constant.units == "second"
-    try:
+def test_time_constant():
+    """Test the LIA Time constant."""
+    start_time_constant = lia.time_constant
+    assert start_time_constant.units == "second"
+    with pytest.raises(ValueError):
         lia.time_constant = 2 * u.second
-    except:
-        print("Invalid time constant succesfully caught in lia")
+
     lia.time_constant = 3 * u.second
-    assert ("Time constant set to %f" % lia.time_constant.magnitude) in file.read()
+    with open(log_filename) as file:
+        string = "Time constant set to %f" % lia.time_constant.magnitude
+        assert string in file.read()
+
+    # Restore
+    lia.time_constant = start_time_constant
